b9748fce857b284db06b241fca152199e3977aa4
ziyinw3 <zwang060503@gmail.com>
2022-11-20 22:42:03 -0600

    trying to add js for button fade in on timer

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+let scroll_button = document.getElementById('scroll_button');
+
+window.addEventListener(onload, () => {
+    scroll_button.classList.toggle("fade-in");
+})
+
+/*let scroll_button = document.getElementById('scroll_button');
+document.getElementById('body').onload = function() {
+    scroll_button.classList.toggle('fade_in');
+} */
