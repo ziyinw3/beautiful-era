d38fea3182058c73595dc08308d51d7d7208a68c
ziyinw3 <zwang060503@gmail.com>
2022-11-23 22:23:26 -0600

    fade in main title text

--- javascript/script.js
+++ javascript/script.js
@@ -4,4 +4,7 @@ function fade_in(id, delay) {
 
 document.addEventListener('DOMContentLoaded', () => {
     fade_in("scroll_button", 2000);
+    fade_in("cn_title", 300);
+    fade_in("en_title", 500);
+    fade_in("subtitle", 600);
 });
