ec7de258a67da36eeac4ed5283df9da1031f145a
ziyinw3 <zwang060503@gmail.com>
2022-11-25 20:07:32 -0600

    test scroll direction listener

--- test/script_test.js
+++ test/script_test.js
@@ -1,6 +1,7 @@
 var opacity = 1;
 element = document.getElementById("element");
 
+
 window.addEventListener("scroll", () => {
     opacity = 1.5 * (element.offsetHeight + element.getBoundingClientRect().top)/element.offsetHeight;
     if (opacity > 1) {
@@ -9,19 +10,36 @@ window.addEventListener("scroll", () => {
     else if (opacity < 0) {
         opacity = 0;
     }
-    console.log(opacity);
+/*     console.log(opacity); */
     element.style.opacity = opacity;
 })
 
+
+
 function scroll_dir_listener() {
-    var scroll_position = 0;
+    var old_scroll_pos = window.scrollY;
     var dir = 'down';
-    if (document.body.getBoundingClientRect().top > scroll_position) {
-        dir = 'up';
+    window.onscroll = function(e) {
+        if (old_scroll_pos < window.scrollY) {
+            dir = 'down';
+        }
+        else {
+            dir = 'up';
+        }
+        old_scroll_pos = window.scrollY;
+        console.log(old_scroll_pos, window.scrollY);
     }
-    else {
-        dir = 'down';
-    }
-    scroll_position = document.body.getBoundingClientRect().top;
-    console.log(dir);
 }
+
+// Initial state
+var scrollPos = 0;
+// adding scroll event
+window.addEventListener('scroll', function(){
+  // detects new state and compares it with the new one
+  if ((document.body.getBoundingClientRect()).top > scrollPos)
+		console.log('up');
+	else
+		console.log('down');
+	// saves the new position for iteration.
+	scrollPos = (document.body.getBoundingClientRect()).top;
+});
