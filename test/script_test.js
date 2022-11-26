97df2bde7f0c4ef2ada8bea0adc0c2e666a93079
ziyinw3 <zwang060503@gmail.com>
2022-11-25 23:03:11 -0600

    integrate scroll listener

--- test/script_test.js
+++ test/script_test.js
@@ -1,45 +1,45 @@
-var opacity = 1;
-element = document.getElementById("element");
+
+var scrollPos = 0;
+var dir = '';
 
 
 window.addEventListener("scroll", () => {
-    opacity = 1.5 * (element.offsetHeight + element.getBoundingClientRect().top)/element.offsetHeight;
+    dir_listener();
+    fade_scroll('element');
+})
+
+// functions
+
+function fade_scroll(id) {
+    element = document.getElementById(id);
+    if (dir = 'down') {    
+        console.log('going down');
+        var opacity = 1;
+        opacity = 3 * (element.offsetHeight + element.getBoundingClientRect().top)/element.offsetHeight;
+    }
+    else if (dir = 'up') {
+        console.log('going up');
+        var opacity = 1;
+        opacity = 1 - 3 * (element.getBoundingClientRect().top - element.offsetHeight)/element.offsetHeight;
+    }
+    // floorer
     if (opacity > 1) {
         opacity = 1;
     }
     else if (opacity < 0) {
         opacity = 0;
     }
-/*     console.log(opacity); */
     element.style.opacity = opacity;
-})
-
-
-
-function scroll_dir_listener() {
-    var old_scroll_pos = window.scrollY;
-    var dir = 'down';
-    window.onscroll = function(e) {
-        if (old_scroll_pos < window.scrollY) {
-            dir = 'down';
-        }
-        else {
-            dir = 'up';
-        }
-        old_scroll_pos = window.scrollY;
-        console.log(old_scroll_pos, window.scrollY);
-    }
+    console.log(opacity);
 }
 
-// Initial state
-var scrollPos = 0;
-// adding scroll event
-window.addEventListener('scroll', function(){
-  // detects new state and compares it with the new one
-  if ((document.body.getBoundingClientRect()).top > scrollPos)
-		console.log('up');
-	else
-		console.log('down');
-	// saves the new position for iteration.
-	scrollPos = (document.body.getBoundingClientRect()).top;
-});
+
+// listen to scroll direction
+function dir_listener() {
+    if ((document.body.getBoundingClientRect()).top > scrollPos)
+    dir = 'up';
+    else
+    dir = 'down';
+    // saves the new position for iteration.
+    scrollPos = (document.body.getBoundingClientRect()).top;
+}
